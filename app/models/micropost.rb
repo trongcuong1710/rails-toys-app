6f79f1e52be53ef80de3dbde55b0b2c4518d2a8d
Trong Cuong <trongcuong@ssf.vn>
2015-11-06 20:56:35 +0700

    fixed wrong field name

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :User
 	validates :content, length: { maximum: 140 },
 						presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :Micropost
 	validates :name, presence: true
-	validates :mail, presence: true
+	validates :email, presence: true
 end
